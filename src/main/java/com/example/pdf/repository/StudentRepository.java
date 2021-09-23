b7d4eff6b34cd3cebfe77f01e753504869c9d7cb
Pedro Lacombe <pedrocarlantonio@gmail.com>
2021-09-23 17:46:03 -0300

    Repository layer with Student repo

---
+++ src/main/java/com/example/pdf/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.example.pdf.repository;
+
+import com.example.pdf.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, String> {
+
+}
