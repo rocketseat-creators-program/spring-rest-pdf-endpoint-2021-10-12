8aea822f9ff62a921ce313c4b6e14e101fc3c6e0
Pedro Lacombe <pedrocarlantonio@gmail.com>
2021-09-23 17:51:37 -0300

    Test default package

---
+++ src/test/java/com/example/pdf/SpringRestPdfEndpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestPdfEndpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
