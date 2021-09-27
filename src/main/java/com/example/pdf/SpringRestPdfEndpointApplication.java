67c4c11358cbf6000afb1cd63918e26bc037a733
Pedro Lacombe <pedrocarlantonio@gmail.com>
2021-09-27 02:49:17 -0300

    Added some Schools to be saved on database when application starts

--- src/main/java/com/example/pdf/SpringRestPdfEndpointApplication.java
+++ src/main/java/com/example/pdf/SpringRestPdfEndpointApplication.java
@@ -1,6 +1,8 @@
 package com.example.pdf;
 
+import com.example.pdf.model.School;
 import com.example.pdf.model.Student;
+import com.example.pdf.repository.SchoolRepository;
 import com.example.pdf.repository.StudentRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -17,18 +19,31 @@ public class SpringRestPdfEndpointApplication {
     }
 
     @Bean
-    CommandLineRunner run(StudentRepository studentRepository) {
+    CommandLineRunner run(StudentRepository studentRepository, SchoolRepository schoolRepository) {
         return args -> {
-            studentRepository.save(new Student("Pedro", "pedro@gmail.com", "123456", LocalDate.of(1992, 8, 28)));
-            studentRepository.save(new Student("Marcos", "marcos@gmail.com", "123456", LocalDate.of(1990, 2, 11)));
-            studentRepository.save(new Student("Paulo", "paulo@gmail.com", "123456", LocalDate.of(1988, 3, 12)));
-            studentRepository.save(new Student("Vinicios", "vinicios@gmail.com", "123456", LocalDate.of(1996, 7, 18)));
-            studentRepository.save(new Student("Jorge", "jorge@gmail.com", "123456", LocalDate.of(2000, 9, 21)));
-            studentRepository.save(new Student("Aline", "aline@gmail.com", "123456", LocalDate.of(1997, 10, 27)));
-            studentRepository.save(new Student("Paula", "paula@gmail.com", "123456", LocalDate.of(1995, 11, 2)));
-            studentRepository.save(new Student("Ana", "ana@gmail.com", "123456", LocalDate.of(1993, 3, 1)));
-            studentRepository.save(new Student("Marcia", "marcia@gmail.com", "123456", LocalDate.of(1964, 8, 9)));
-            studentRepository.save(new Student("Marta", "marta@gmail.com", "123456", LocalDate.of(1996, 8, 9)));
+            School ignite = new School("Ignite");
+            School discover = new School("Discover");
+            School expertsClub = new School("ExpertsClub");
+
+            schoolRepository.save(ignite);
+            schoolRepository.save(discover);
+            schoolRepository.save(expertsClub);
+
+            studentRepository.save(new Student("Pedro", "pedro@gmail.com", "123456", LocalDate.of(1992, 8, 28), discover));
+            studentRepository.save(new Student("Marcos", "marcos@gmail.com", "123456", LocalDate.of(1990, 2, 11), discover));
+            studentRepository.save(new Student("Paulo", "paulo@gmail.com", "123456", LocalDate.of(1988, 3, 12), discover));
+            studentRepository.save(new Student("Vinicios", "vinicios@gmail.com", "123456", LocalDate.of(1996, 7, 18), discover));
+
+            studentRepository.save(new Student("Jorge", "jorge@gmail.com", "123456", LocalDate.of(2000, 9, 21), ignite));
+            studentRepository.save(new Student("Aline", "aline@gmail.com", "123456", LocalDate.of(1997, 10, 27), ignite));
+            studentRepository.save(new Student("Paula", "paula@gmail.com", "123456", LocalDate.of(1995, 11, 2), ignite));
+
+            studentRepository.save(new Student("Ana", "ana@gmail.com", "123456", LocalDate.of(1993, 3, 1), expertsClub));
+            studentRepository.save(new Student("Marcia", "marcia@gmail.com", "123456", LocalDate.of(1964, 8, 9), expertsClub));
+            studentRepository.save(new Student("Marta", "marta@gmail.com", "123456", LocalDate.of(1996, 1, 5), expertsClub));
+            studentRepository.save(new Student("Yuri", "yuri@gmail.com", "123456", LocalDate.of(2001, 3, 25), expertsClub));
+            studentRepository.save(new Student("Rodrigo", "rodrigo@gmail.com", "123456", LocalDate.of(2003, 5, 19), expertsClub));
+            studentRepository.save(new Student("Marcelo", "marcelo@gmail.com", "123456", LocalDate.of(1995, 7, 11), expertsClub));
         };
     }
 
