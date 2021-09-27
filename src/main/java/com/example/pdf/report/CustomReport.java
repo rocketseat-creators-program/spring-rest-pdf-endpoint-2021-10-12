0386e20ef1bd86bc6c77a10bea41b1162bba2670
Pedro Lacombe <pedrocarlantonio@gmail.com>
2021-09-27 02:51:30 -0300

    Change the implementation of CustomReport

--- src/main/java/com/example/pdf/report/CustomReport.java
+++ src/main/java/com/example/pdf/report/CustomReport.java
@@ -9,9 +9,7 @@ import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
 import com.itextpdf.layout.element.Text;
-import com.itextpdf.layout.property.HorizontalAlignment;
 import com.itextpdf.layout.property.TextAlignment;
-import com.itextpdf.layout.property.VerticalAlignment;
 import org.springframework.util.ObjectUtils;
 
 import java.io.ByteArrayInputStream;
@@ -22,101 +20,85 @@ import java.util.List;
 public class CustomReport {
 
     private Table table;
+    private PageSize pageSize;
     private Document document;
     private PdfDocument pdfDocument;
     private ByteArrayOutputStream baos;
 
-    private CustomReport() {
+    public CustomReport() {
         this.baos = new ByteArrayOutputStream();
         this.pdfDocument = new PdfDocument(new PdfWriter(this.baos));
         this.document = new Document(this.pdfDocument);
     }
 
-    public static CustomReport builder() {
-        return new CustomReport();
+    public void pageSize(PageSize pageSize) {
+        this.pageSize = pageSize;
+        this.pdfDocument.setDefaultPageSize(pageSize);
     }
 
-    public CustomReport landscape(PageSize pageSize) {
-        this.pdfDocument.setDefaultPageSize(pageSize.rotate());
-        return this;
+    public void landscape() {
+        this.pdfDocument.setDefaultPageSize(ObjectUtils.isEmpty(this.pageSize) ? PageSize.Default.rotate() : this.pageSize.rotate());
     }
 
-    public CustomReport addNewLine() {
+    public void addNewLine() {
         this.document.add(new Paragraph("\n"));
-        return this;
     }
 
-    public CustomReport addParagraph(String text) {
+    public void addParagraph(String text) {
         CustomText customText = CustomText.builder().value(text).build();
-        return this.addParagraph(customText);
+        this.addParagraph(customText);
     }
 
-    public CustomReport addParagraph(CustomText text) {
+    public void addParagraph(CustomText text) {
         this.document.add(this.defaultParagraph(text));
-        return this;
     }
 
-    public ByteArrayInputStream build() {
+    public void closeDocument() {
         this.document.close();
-        return new ByteArrayInputStream(this.baos.toByteArray());
     }
 
-    public CustomReport openTable(Integer columnWidth) {
+    public void openTable(Integer columnWidth) {
         this.table = new Table(columnWidth);
         this.table.useAllAvailableWidth();
         this.table.setTextAlignment(TextAlignment.CENTER);
-
-        return this;
     }
 
-    public CustomReport addTableHeader(CustomText header) {
+    public void addTableHeader(CustomText header) {
         this.validateTable();
         this.table.addHeaderCell(this.defaultParagraph(header));
-        return this;
     }
 
-    public CustomReport addTableHeader(String header) {
+    public void addTableHeader(String header) {
         CustomText customText = CustomText.builder().value(header).build();
-        return this.addTableHeader(customText);
+        this.addTableHeader(customText);
     }
 
-    public CustomReport addTableHeaders(CustomText... headers) {
+    public void addTableHeaders(CustomText... headers) {
         for (CustomText customText : headers) {
             this.addTableHeader(customText);
         }
-
-        return this;
     }
 
-    public CustomReport addTableHeaders(String... headers) {
+    public void addTableHeaders(String... headers) {
         for (String header : headers) {
             this.addTableHeader(header);
         }
-
-        return this;
     }
 
-    public CustomReport addTableRow(Object[] columns) {
+    public void addTableRow(List<Object> columns) {
         this.validateTable();
-        for (int i = 0; i < columns.length; i++) {
-            this.table.addCell(columns[i].toString());
+        for (Object column : columns) {
+            this.table.addCell(column.toString());
         }
-
-        return this;
     }
 
-    public CustomReport addTableRows(List<Object[]> rows) {
-        for (Object[] objects : rows) {
-            this.addTableRow(objects);
-        }
-
-        return this;
-    }
-
-    public CustomReport closeTable() {
+    public void closeTable() {
         this.validateTable();
         this.document.add(this.table);
-        return this;
+    }
+
+    public ByteArrayInputStream getByteArrayInputStream() {
+        return new ByteArrayInputStream(this.baos.toByteArray());
     }
 
     private Paragraph defaultParagraph(CustomText customText) {
@@ -145,9 +127,6 @@ public class CustomReport {
         }
 
         paragraph.add(text);
-        paragraph.setVerticalAlignment(VerticalAlignment.MIDDLE);
-        paragraph.setHorizontalAlignment(HorizontalAlignment.CENTER);
-
         return paragraph;
     }
 
