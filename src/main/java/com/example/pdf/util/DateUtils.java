c6b45579088e6d1a53dd9803e4ee4f39d8964c13
Pedro Lacombe <pedrocarlantonio@gmail.com>
2021-09-27 02:50:49 -0300

    Implemented functions to format LocalDate and LocalDateTime

--- src/main/java/com/example/pdf/util/DateUtils.java
+++ src/main/java/com/example/pdf/util/DateUtils.java
@@ -3,9 +3,19 @@ package com.example.pdf.util;
 import org.springframework.util.ObjectUtils;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.Period;
+import java.time.format.DateTimeFormatter;
 
 public class DateUtils {
+
+    public static String format(LocalDate date, String pattern) {
+        return DateTimeFormatter.ofPattern(pattern).format(date);
+    }
+
+    public static String format(LocalDateTime date, String pattern) {
+        return DateTimeFormatter.ofPattern(pattern).format(date);
+    }
 
     public static int age(LocalDate birthday) {
         if (!ObjectUtils.isEmpty(birthday)) {
